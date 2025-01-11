526a573765669b797177e72abf291a9735c4806c
raapi <raafi.hafidz75@smk.belajar.id>
2025-01-11 11:06:51 +0700

    Update array01.php

--- array/array01.php
+++ array/array01.php
@@ -1,9 +1,9 @@
 <?php
 $buah = ["apel","mangga","jeruk"];
 
-echo $buah[0].PHP_EOL
-echo $buah[1].PHP_EOL
-echo $buah[2].PHP_EOL
+echo $buah[0].PHP_EOL;
+echo $buah[1].PHP_EOL;
+echo $buah[2].PHP_EOL;
 
 $campur = ["ayam",1,true,1,5];
 var_dump($campur);
